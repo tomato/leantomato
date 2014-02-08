5229801106bf5009cfd01381d5c038c8742ac02f
Tom <tom@tomhowlett.com>
2014-02-08 16:38:08 +0000

    moved to a single page with a menu

--- js/site.js
+++ js/site.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   $('.logo').lettering();
+  $('.menu-container a').click(function(e){
+    var $linkId = $(this).data('link')
+    var $target = $('#' + $linkId)
+    if($linkId.length > 0 && $target.length > 0) {
+      $("html, body").animate({ scrollTop: $target.offset().top - 50}, 1000);
+      e.preventDefault();
+    }
+  })
 }); 
