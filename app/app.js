117fdc652ba54d36d6e5dc332f84072643bde575
Tom <tom@tomhowlett.com>
2015-07-30 12:24:15 +0100

    added some content and got the about us link working

--- app/app.js
+++ app/app.js
@@ -1,64 +1,85 @@
 (function(document, $){
 
-  var expandColumn = function()
-  {
+  var rotateTitle = function($element){
+    $($element)
+      .velocity({
+        rotateZ: "90deg",
+        'transform-origin': 'bottom left',
+        translateY: "-30px",
+        translateX: "-30px",
+        width: "800px"
+      });
+  };
+
+  var unRotateTitle = function($element){
+    $($element)
+      .velocity({
+        rotateZ: "0deg",
+        'transform-origin': 'bottom left',
+        translateY: "0px",
+        translateX: "0px",
+        width: "400px"
+      });
+  };
+
+  var expandDivision = function($toExpand,$toShrink ){
+    $toShrink.velocity({ width: '10%'}).addClass('shrunk');
+    $toExpand.velocity({ width: '85%'}).addClass('expanded');
+  };
+
+  var expand = function($toExpand,$toShrink){
+    expandDivision($toExpand, $toShrink);
+    rotateTitle($('h2',$toShrink));
+    $('.hide-when-collapsed',$toShrink).velocity("fadeOut");
+    $('.show-when-expanded',$toExpand).velocity("fadeIn");
+    $('.menu-content').velocity("fadeOut");
+  };
+
+  var equalise = function($elements){
+
+    $shrunks = $elements.filter('.shrunk');
+
+    $elements
+      .velocity({ width: '47%'})
+      .removeClass('shrunk expanded');
+
+    $shrunks.each(function(i, e){
+      $element = $(e);
+      unRotateTitle($('h2',$element));
+      $('.hide-when-collapsed',$element).velocity("fadeIn");
+    });
+
+    $('.show-when-expanded').velocity("fadeOut");
+    $('.menu-content').velocity("fadeOut");
+  };
+
+
+  var expandColumn = function(){
+    var $both = $('.intro .column');
     var $clickedE = $(this);
-    var $otherE = $('.intro .column').not($clickedE);
+    var $otherE = $both.not($clickedE);
 
-    var rotateTitle = function($element){
-      $($element)
-        .velocity({
-          rotateZ: "90deg",
-          'transform-origin': 'bottom left',
-          translateY: "-30px",
-          translateX: "-30px",
-          width: "800px"
-        });
-    };
-
-    var unRotateTitle = function($element){
-      $($element)
-        .velocity({
-          rotateZ: "0deg",
-          'transform-origin': 'bottom left',
-          translateY: "0px",
-          translateX: "0px",
-          width: "400px"
-        });
-    };
-
-    var equaliseDivision = function($toExpand,$toShrink ){
-      $toShrink.add($toExpand)
-        .velocity({ width: '47%'})
-        .removeClass('shrunk expanded');
-    };
-
-    var expandDivision = function($toExpand,$toShrink ){
-      $toShrink.velocity({ width: '10%'}).addClass('shrunk');
-      $toExpand.velocity({ width: '85%'}).addClass('expanded');
-    };
-
-    var expand = function($toExpand,$toShrink){
-      expandDivision($toExpand, $toShrink);
-      rotateTitle($('h2',$toShrink));
-      $('.hide-when-collapsed',$toShrink).velocity("fadeOut");
-      $('.show-when-expanded',$toExpand).velocity("fadeIn");
-    };
-
-    var equalise = function($shrunk,$expanded){
-      equaliseDivision($expanded, $shrunk);
-      unRotateTitle($('h2',$shrunk));
-      $('.hide-when-collapsed',$shrunk).velocity("fadeIn");
-      $('.show-when-expanded').velocity("fadeOut");
-    };
 
     if(!$clickedE.hasClass('expanded'))
     {
       if($clickedE.hasClass('shrunk'))
-        equalise($clickedE, $otherE);
+        equalise($both);
       else
         expand($clickedE, $otherE);
     }
+  };
+
+  var shrinkColumn = function($toShrink){
+    $toShrink.velocity({ width: '10%'}).addClass('shrunk').removeClass('expanded');
+    rotateTitle($('h2',$toShrink));
+    $('.hide-when-collapsed',$toShrink).velocity("fadeOut");
+  };
+
+  var insertInCenter = function($insertedElement)
+  {
+    $('.show-when-expanded').velocity("fadeOut");
+    shrinkColumn($('.intro .column'));
+    $insertedElement.velocity('fadeIn');
   };
 
   var vTransform = function($element, transformation, apply)
@@ -109,6 +130,7 @@
   $(function(){
     $('.intro .column').click(expandColumn);
     $('.logo h1').on({ mouseover: textEncrypt, mouseout: textDecrypt });
+    $('.about-us-link').click(function(){ insertInCenter($('.about-us-content')); });
   });
 
 
